b6981be92fa3ebb64cae277bfbe1dbc14acf292f
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-07-06 19:01:01 +0200

    Add transparency

--- shinyapp/R/plot_theme.R
+++ shinyapp/R/plot_theme.R
@@ -3,6 +3,14 @@ plot_theme <- function(){
   
   theme(
     
+    # -- backgrounds
+    panel.background = element_blank(),
+    plot.background = element_blank(),
+    
+    # -- panel
+    panel.grid.minor = element_blank(),
+    
+    # -- axis
     axis.title = element_blank()
     
   )
