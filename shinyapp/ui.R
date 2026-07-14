0db3b1c11f69404a638e34fb7fc675b7e440bfb7
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-07-14 15:00:45 +0200

    Fixed #54

--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -1,34 +1,40 @@
 
 
 # Define UI for application that draws a histogram
-page_navbar(
+tagList(
   
-  # -- parameters
-  title = "GPX-Tracker",
-  window_title = "GPX-Tracker",
-  id = "nav",
-  footer = p(style = "font-size:9pt;margin-left:20px;", paste("©", format(Sys.Date(), "%Y"), "Philippe Peret", "|", paste0("Version", app_version))),
-  
-  tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "css/slider.css"),
-    tags$link(rel = "stylesheet", type = "text/css", href = "css/gpx_tracker.css")),
+  # -- keep it outside
   shinyjs::useShinyjs(),
   
-  # -- theme
-  theme = bs_theme(
-    bg = "#e8e5d4",
-    fg = "#000",
-    base_font = font_google("Quicksand")),
-  
-  # -- content
-  # by default, the ui contains only the home tab
-  nav_panel(
-    title = "Home",
-    value = "home",
-    icon = icon("home"),
+  # -- actual UI
+  page_navbar(
     
-    h1("Tracks"),
-    p("Available GPX files:"),
-    uiOutput("file_selector"))
-  
-)
+    # -- parameters
+    title = "GPX-Tracker",
+    window_title = "GPX-Tracker",
+    id = "nav",
+    footer = p(style = "font-size:9pt;margin-left:20px;", paste("©", format(Sys.Date(), "%Y"), "Philippe Peret", "|", paste0("Version", app_version))),
+    
+    # -- custom CSS
+    header = tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "css/slider.css"),
+      tags$link(rel = "stylesheet", type = "text/css", href = "css/gpx_tracker.css")),
+    
+    # -- theme
+    theme = bs_theme(
+      bg = "#e8e5d4",
+      fg = "#000",
+      base_font = font_google("Quicksand")),
+    
+    # -- content
+    # by default, the ui contains only the home tab
+    nav_panel(
+      title = "Home",
+      value = "home",
+      icon = icon("home"),
+      
+      h1("Tracks"),
+      p("Available GPX files:"),
+      uiOutput("file_selector"))
+    
+  ))
