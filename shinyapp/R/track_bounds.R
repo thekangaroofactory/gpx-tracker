d30e3d4d0fcd16b5e8f1a7dd74da2580d7dbc4f0
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-07-25 16:33:08 +0200

    Fixed #92

---
+++ shinyapp/R/track_bounds.R
@@ -0,0 +1,19 @@
+
+
+#' Track Bounds
+#'
+#' @param segments the segment table
+#'
+#' @returns a data.frame of the first and last rows
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' track_bounds(segments)
+#' }
+
+track_bounds <- function(segments){
+
+  segments |> filter(row_number() %in% c(1, n()))
+  
+}
