4dc7877603db6be741f1a5d36a6c0a2943627a4e
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-07-25 16:41:42 +0200

    Fixed #93

---
+++ shinyapp/R/bounds_popup.R
@@ -0,0 +1,23 @@
+
+
+#' Add Bounds Popup
+#'
+#' @param bounds the bounds segment table
+#'
+#' @returns a data.frame with popup column added
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' bounds_popup(track_bounds(segments))
+#' }
+
+bounds_popup <- function(bounds){
+  
+  bounds |>
+    
+    mutate(popup = paste(sep = "<br/>",
+                   paste0("<b>", label, "</b>"),
+                   format(if(label == "Start") datetime_start else datetime_end,'%Y-%m-%d, %H:%M:%S')))
+  
+}
