bb43c73752bd4bfe25b77aad4e12549ca9f5248c
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-07-26 13:15:28 +0200

    Fixed #110

--- shinyapp/R/planning/leg_targets.R
+++ shinyapp/R/planning/leg_targets.R
@@ -11,7 +11,7 @@
 #' @param max the maximum distance for the leg
 #' @param step the step to compute sequence between min and max
 #'
-#' @returns a vector of segment ids
+#' @returns a data.frame of targets
 #' @export
 #'
 #' @examples
@@ -25,6 +25,16 @@ leg_targets <- function(segments, start = 1, min = 70, max = 80, step = 5){
   ref_dist <- segments |> filter(segment_id == start) |> pull(cum_distance)
   
   # -- compute targets
-  unlist(lapply(seq(from = min, to = max, by = step), function(x) which.min(abs(segments$cum_distance - ref_dist - x))))
+  idx <- unlist(lapply(seq(from = min, to = max, by = step), function(x) which.min(abs(segments$cum_distance - ref_dist - x))))
+  
+  # -- return
+  segments |> 
+    filter(segment_id %in% idx) |>
+    select(segment_id, geometry_end, datetime_end, elevation_end, cum_distance) |>
+    mutate(type = "target",
+           distance = cum_distance - ref_dist) |>
+    rename(geometry = geometry_end,
+           datetime = datetime_end,
+           elevation = elevation_end)
   
 }
