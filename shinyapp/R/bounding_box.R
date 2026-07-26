f3037ac76325c869996dc459a516a201fbf49882
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-07-26 13:12:56 +0200

    Fixed #107

--- shinyapp/R/bounding_box.R
+++ shinyapp/R/bounding_box.R
@@ -4,9 +4,9 @@ bounding_box <- function(segments){
   
   # -- return
   c(
-    lng1 = min(st_coordinates(segments$geometry_start)[,1]),
-    lat1 = min(st_coordinates(segments$geometry_start)[,2]),
-    lng2 = max(st_coordinates(segments$geometry_end)[,1]),
-    lat2 = max(st_coordinates(segments$geometry_end)[,2]))
+    lng1 = min(st_coordinates(segments$geometry)[,1]),
+    lat1 = min(st_coordinates(segments$geometry)[,2]),
+    lng2 = max(st_coordinates(segments$geometry)[,1]),
+    lat2 = max(st_coordinates(segments$geometry)[,2]))
   
 }
