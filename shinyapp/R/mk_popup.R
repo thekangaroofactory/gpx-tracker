ace5e5ba3c2d5dc1ff46100e2e70c3e012c846d3
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2026-07-26 13:14:45 +0200

    Fixed #109

---
+++ shinyapp/R/mk_popup.R
@@ -0,0 +1,77 @@
+
+
+mk_popup <- function(markers, info = c("title", "datetime", "show_targets", "clear_targets"), ns = NULL){
+  
+  # -- build popup
+  popup <- paste0("<b>", ktools::toupperfirst(markers$type), "</b>")
+  
+  # ----------------------------------------------------------------------------
+  # datetime
+  # ----------------------------------------------------------------------------
+  
+  if("datetime" %in% info)
+    popup <- paste(sep = "<br/>", popup, format(markers$datetime, '%Y-%m-%d, %H:%M:%S'))
+  
+  
+  # ----------------------------------------------------------------------------
+  # action: show (leg) targets
+  # ----------------------------------------------------------------------------
+  
+  if("show_targets" %in% info){
+    
+    # -- actionLink
+    helper <- function(x)
+      lapply(x, function(x)
+        paste(actionLink(inputId = ns(paste0("show_targets_", x)),
+                         label = "Show",
+                         onclick = paste0('Shiny.setInputValue(\"', ns("leg_targets"), '\", this.id, {priority: \"event\"})'))))
+    
+    popup <- paste(popup, paste(helper(markers$segment_id), "leg targets."), sep = "<br/>")
+    
+  }
+  
+  
+  # ----------------------------------------------------------------------------
+  # action: clear (leg) targets
+  # ----------------------------------------------------------------------------
+
+  if("clear_targets" %in% info){
+    
+    # -- actionLink
+    helper <- function(x)
+      lapply(x, function(x)
+        paste(actionLink(inputId = ns(paste0("leg_target_", x)),
+                         label = "Clear",
+                         onclick = paste0('Shiny.setInputValue(\"', ns("clear_group"), '\", this.id, {priority: \"event\"})'))))
+    
+    popup <- paste(popup, paste(helper(markers$segment_id), "leg targets."), sep = "<br/>")
+    
+  }
+  
+  
+  # ----------------------------------------------------------------------------
+  # action: add (leg)
+  # ----------------------------------------------------------------------------
+  
+  if("add_leg" %in% info){
+    
+    # -- actionLink
+    helper <- function(x)
+      lapply(x, function(x)
+        paste(actionLink(inputId = ns(paste0("add_leg_", x)),
+                         label = "Add",
+                         onclick = paste0('Shiny.setInputValue(\"', ns("create_leg"), '\", this.id, {priority: \"event\"})'))))
+    
+    popup <- paste(popup, paste(helper(markers$segment_id), "leg."), sep = "<br/>")
+    
+  }
+  
+
+  # ----------------------------------------------------------------------------
+  # update markers & return
+  # ----------------------------------------------------------------------------
+  
+  markers |>
+    mutate(popup = popup)
+  
+}
